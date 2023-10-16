aa37bddfd484a42a67337c8232bd40b136bd6fee
AntonTzy <143946519+AntonTzy@users.noreply.github.com>
2023-10-16 12:40:54 +0700

    Delete Tugas Bu Ade directory

--- Tugas Bu Ade/tipeKarakter.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char huruf;
-    huruf ='z';
-    printf("Huruf yang tersimpan adalah: %c", huruf);
-    printf("\n");
-    return 0;
-}
--- Tugas Bu Ade/tugas1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Saya sedang belajar bahasa C");
-
-    return 0;
-}
